16c777f0b60e7322d4846f05a90bc19fcf15d9c8
Ali Cagatay <aliccagatay@gmail.com>
2025-06-05 14:44:11 +0100

    Add About page component with basic layout

---
+++ app/about.tsx
@@ -0,0 +1,22 @@
+import { Text, View } from "react-native";
+
+export default function About() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text
+        style={{
+          fontSize: 30,
+          color: "#333",
+        }}
+      >
+        About Page
+      </Text>
+    </View>
+  );
+}
