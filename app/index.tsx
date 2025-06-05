d288b3881b76bfb82e7dadb6b377e61b4be871f8
Ali Cagatay <aliccagatay@gmail.com>
2025-06-05 14:44:16 +0100

    Update index screen to include welcome message and navigation link to About page

--- app/index.tsx
+++ app/index.tsx
@@ -1,5 +1,7 @@
 import { Text, View } from "react-native";
 
+import { Link } from "expo-router";
+
 export default function Index() {
   return (
     <View
@@ -9,7 +11,20 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <Text
+        style={{
+          fontSize: 30,
+          color: "#333",
+        }}
+      >
+        Welcome to the Expo Router!
+      </Text>
+      <Link
+        href="/about"
+        style={{ marginTop: 20, fontSize: 20, color: "#007AFF" }}
+      >
+        Go to About Page
+      </Link>
     </View>
   );
 }
